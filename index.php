a8f484e61260c58cb73dfef98993a6098341075b
Arraxy <Arraxy@ya.ru>
2017-09-18 10:26:26 +0600

    Add index.php file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="/css/flexbox.css" rel="stylesheet" >
+	<link href="/css/flexbox_page.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="flex-container">
+		<div class="flex-element header">
+			<h2>Title</h2>
+		</div>
+		
+		<div class="flex-element wrapper">
+			<div class="flex-element sidemenu">
+                <h2>Sidemenu</h2>
+			</div>
+			
+            <div class="flex-element content">
+                <h2>Content</h2>
+			</div>
+		</div>
+		
+		<div class="flex-element footer">
+            <h2>Footer</h2>
+		</div>
+	</div>
+</body>
+
+</html>
